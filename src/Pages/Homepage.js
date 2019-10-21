7131645e148ed66a922094a656ce55464e8415d8
kch8293 <casey.hoh.kc@gmail.com>
2019-10-21 17:31:29 +0800

    fixed redirects

--- src/Pages/Homepage.js
+++ src/Pages/Homepage.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button } from "reactstrap";
+import { Button, Row, Col } from "reactstrap";
 import UserImages from "../Containers/UserImages";
 
 class Homepage extends React.Component {
@@ -11,21 +11,25 @@ class Homepage extends React.Component {
             key={user.id}
             className="row border-0 mb-5 rounded pt-3 pb-3 Homepage_UserBox"
           >
-            <li style={{ listStyleType: "none" }} className="col-2">
-              <div className="row">
-                {user.id}: {user.username}
-              </div>
-              <img
-                src={user.profileImage}
-                alt="user profile"
-                className="row rounded-circle Homepage_ProfileImage"
-              ></img>
-              <a href={`/user/${user.id}`}>
-                <Button color="primary" className="row">
-                  See More
-                </Button>
-              </a>
-            </li>
+            <Col lg="2">
+              <li style={{ listStyleType: "none" }}>
+                <Row lg="2">
+                  {user.id}: {user.username}
+                </Row>
+                <Row>
+                  <img
+                    src={user.profileImage}
+                    alt="user profile"
+                    className="rounded-circle Homepage_ProfileImage"
+                  />
+                </Row>
+                {/* <Row> */}
+                <a href={`/user/${user.id}`}>
+                  <Button color="primary">See More</Button>
+                </a>
+                {/* </Row> */}
+              </li>
+            </Col>
             <div className="col">
               <UserImages id={user.id}></UserImages>
             </div>
